50fc2ddad6dd34c6d12b5d04b8ac04642ab09832
salitordjman <salitorgi@gmail.com>
2021-12-11 17:23:32 +0200

    end 11.12.21 17:30

--- covid-JS-project-appleseeds.js
+++
@@ -1,7 +0,0 @@
-const dataCovid= document.querySelector(".covid")
-async function fetching ()  {
-  let test = await (await fetch('https://corona-api.com/countries')).json()
-  console.log(test)
-  dataCovid.innerHTML=test
-}
-fetching()
